bf4eadcdb735a8157bd19ad64dc83126b5e82e73
Bird <bird@gmail.com>
2017-01-16 23:29:30 +0800

    init

--- pages/create/create.js
+++ pages/create/create.js
@@ -2,9 +2,18 @@
 //获取应用实例
 var app = getApp();
 Page({
+    bindFormSubmit: function(e){
+        var text = e.detail.value.todoText;
+        var todos = wx.getStorageSync('todos');
+        todos.push({id: todos[todos.length - 1].id, text: text, createTime: new Date()});
+        wx.setStorageSync('todos', todos);
+        wx.redirectTo({
+            url: '../index/index'
+        }); 
+    },
     cancel: function(){
        wx.redirectTo({
-        url: '../index/index'
-    }); 
+            url: '../index/index'
+        }); 
     }
 });
