88bf1b3a122a07b44a3d777509a401cff774ddd2
Bird <bird@gmail.com>
2017-01-16 22:30:15 +0800

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+//app.js
+App({
+  onLaunch: function () {
+    var number = 5;
+    var todos = [];
+    for (var index = 0; index < number; index++){
+      todos.push({id: index, text: 'it is todo ' + (index+1), createTime: new Date()});
+    }
+    wx.setStorageSync('todos', todos)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  }
+})
