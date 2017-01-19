8ebb2d3a5e0f76cd0ec13c94ab42406833d3d312
Bird <bird@gmail.com>
2017-01-19 22:39:08 +0800

    support tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,28 +7,19 @@ Page({
     todos: []
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
     // get todo list
     var todos = wx.getStorageSync('todos');
     todos.forEach(function(todo){
         todo.createTimeText = util.formatTime(todo.createTime);
         todo.isDelete = false;
     });
-    that.setData({todos: todos});
+    this.setData({todos: todos});
   },
   onShareAppMessage: function(){
     return {
       title: '分享我的TODO',
       desc: '我的TODO列表',
-      path: '/page/user?id=123'
+      path: '/page/index'
     }
   },
   createTodo: function(){
